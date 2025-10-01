75689d6ec8e135849b3c092cec59283c3881be6c
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2025-10-01 12:26:24 +0200

    Añadido listado de mesas del restaurante

--- src/feature/table-list.ts
+++ src/feature/table-list.ts
@@ -3,3 +3,9 @@ numero: number;
 capacidad: number;
 ocupada: boolean;
 }
+
+let mesas: Mesa[] = [
+{ numero: 1, capacidad: 2, ocupada: false },
+{ numero: 2, capacidad: 4, ocupada: true },
+{ numero: 3, capacidad: 6, ocupada: false }
+];
