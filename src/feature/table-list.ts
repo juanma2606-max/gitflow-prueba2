4cc421ed6295bea1a5374c80b11f02aed2434758
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2025-10-01 12:40:11 +0200

    Implementada función mostrarMesasDisponibles

--- src/feature/table-list.ts
+++ src/feature/table-list.ts
@@ -9,3 +9,12 @@ let mesas: Mesa[] = [
 { numero: 2, capacidad: 4, ocupada: true },
 { numero: 3, capacidad: 6, ocupada: false }
 ];
+
+export function mostrarMesasDisponibles(): void {
+    console.log(" Mesas disponibles:");
+    mesas.forEach(mesa => {
+    if (!mesa.ocupada) {
+        console.log(`Mesa ${mesa.numero} (Capacidad: ${mesa.capacidad})`);
+    }
+    });
+}
