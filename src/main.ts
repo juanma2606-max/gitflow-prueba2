c6a578b48c02a86744d3fa7786d5b1c727d214a2
juanma2606 <jsangam2606@iescarrillo.es>
2025-10-22 12:14:17 +0200

    borrado de procesarReserva

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,3 @@ mostrarMesasDisponibles();
 } else {
 console.log("Acceso denegado ");
 }
-export function procesarReserva(cliente: string, total: number): string {
- console.log(`Procesando reserva para ${cliente}.`);
- let descuento = 0;
- let puntosGanados = Math.floor(total / 10);
-
- if (cliente === "Juan Pérez") {
- descuento = total * 0.1;
- total -= descuento;
- console.log(` Descuento aplicado de ${descuento.toFixed(2)} €`);
- }
- 
- console.log(`Total a pagar: ${total} €`);
- console.log(` El cliente ${cliente} ha ganado ${puntosGanados} puntos.`);
- return "Reserva completada correctamente con puntos de recompensa y con descuento.";
-
-}
